dea4e9e05815e3da2a6d39866f62626a381fdc8e
MDDIDAR <mddidar366@gmail.com>
2017-02-11 22:00:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('main-text');
 
 element.innerHTML='new value';//to change the value
 
-var img=document.getElementById('madi');
+var img=document.getElementById('modi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
