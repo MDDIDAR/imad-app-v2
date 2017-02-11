6093dcde76593e06eb980c82657bc9df3e733f1b
MDDIDAR <mddidar366@gmail.com>
2017-02-11 16:28:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleNname=article-one
     //articles[articleName]={}content object for article one
-    var articlesName=req.params.articleName;
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
