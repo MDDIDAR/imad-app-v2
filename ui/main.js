a77b9a274a216df054188da360ac2373e75a9bc3
MDDIDAR <mddidar366@gmail.com>
2017-02-12 11:38:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-
-element.innerHTML='hey here u get the most beautiful girls pic';
-//to change the value
-
-
+//counter code
+var button=document.getElementById('count');
+var counter=0;
+button.onclick=function(){
+    //make a request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the current span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
