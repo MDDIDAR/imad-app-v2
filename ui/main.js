e18d8e361ad80f98ca2579edddf024979510f243
MDDIDAR <mddidar366@gmail.com>
2017-02-12 12:06:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    //make a request to the counter endpoint
+    //create a request objet
+    var request=new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take the action
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                 span.innerHTML=counter.toString();
+            }
+        }
     
-    //render the variable in the current span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    //not done yet
+    };
+    //make the request
+    request.open('GET','http://mddidar.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
