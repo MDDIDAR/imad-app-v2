b4bec21f2998c88d3235b5cd12df1f25ea70062b
MDDIDAR <mddidar366@gmail.com>
2017-02-13 23:29:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,33 @@ button.onclick=function(){
 
 
 //submsubmit-btn
+var submit=document.getElementById('submit-btn');
+   submit.onclick=function(){
+    //create a request object
+var request=new XMLHttpRequest();
+
+//capture the response and store it in a variable
+request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+
+             //take the action
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i< names.length;i++){
+                    list+='<li>'+ names[i] +'</li>';
+                }
+            
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list; 
+            }
+        }
+    //not yet done
+};
+    //make the request
+    var nameinput=document.getElementById('name');
+    var name=nameinput.value;
+    request.open('GET','http://mddidar.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+};
