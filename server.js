4a91d3948ed8fb09f405212c566e1bab0e0ed9cf
MDDIDAR <mddidar366@gmail.com>
2017-02-19 11:43:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,15 +12,12 @@ var articles ={
     heading:'Article One',
     date:'Feb10,2017',
     content:`
-                    <p>
-                        This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.
-                    </p>
-                    <p>
-                        This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.
-                    </p>
-                    <p>
-                        This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.This is my first webapp.
-                    </p>`
+                    <form action='' method='get'>  <input type='hidden' name='L' value='7' />  <input type='submit' value='Toggle Red' /> </form>
+<form action='' method='get'>  <input type='hidden' name='L' value='6' />  <input type='submit' value='Toggle Green' /> </form>
+<form action='' method='get'>
+ <input type='hidden' name='L' value='5' />  <input type='submit' value='Toggle Blue' /> </form>
+<form action='' method='get'>  <input type='range' name='S' min='0' max='1000' step='100' value='0'/>  <input type='submit' value='Set Frequency' /> </form>
+
 },
 'article-two':{
    title:'Article two | md didar',
