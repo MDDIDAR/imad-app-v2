12fa3d2430f7034d32a72565f23b539e7e85223a
MDDIDAR <mddidar366@gmail.com>
2017-02-19 11:46:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var articles ={
 <form action='' method='get'>  <input type='hidden' name='L' value='6' />  <input type='submit' value='Toggle Green' /> </form>
 <form action='' method='get'>
  <input type='hidden' name='L' value='5' />  <input type='submit' value='Toggle Blue' /> </form>
-<form action='' method='get'>  <input type='range' name='S' min='0' max='1000' step='100' value='0'/>  <input type='submit' value='Set Frequency' /> </form>
+<form action='' method='get'>  <input type='range' name='S' min='0' max='1000' step='100' value='0'/>  <input type='submit' value='Set Frequency' /> </form>`
 
 },
 'article-two':{
