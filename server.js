365a6110a3d4e2d3f29e6f095addb38478dc57bd
MDDIDAR <mddidar366@gmail.com>
2017-04-06 08:59:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err, result){
         if(err){
-            req.status(500).send(err.toString());
+            res.status(500).send(err.toString());
         }else{
             req.send(JSON.stringify(result.rows));
         }
