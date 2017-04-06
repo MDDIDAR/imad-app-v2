ac6a11593e78c09b48b04b1c4515d040c1f7bf8e
MDDIDAR <mddidar366@gmail.com>
2017-04-06 08:55:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/test-db',function(req,res){
         if(err){
             req.status(500).send(err.toString());
         }else{
-            req.send(JSON.stringify(result));
+            req.send(JSON.stringify(result.rows));
         }
     });
 });
